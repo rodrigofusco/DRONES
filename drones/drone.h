0f9538e55a8c1d45372dcb4efe9ac02bffbf292d
Dakeri95 <salvador.costa123.sc@gmail.com>
2024-05-14 17:59:32 +0100

    Update drone.h

--- drones/drone.h
+++ drones/drone.h
@@ -1,19 +1,70 @@
+/*drone.h*/
 
-typedef struct _drone *_drone;
+#ifndef DRONE_H_
+#define DRONE_H_
 
+/* Tipo de dados: drone */
+typedef struct _drone* drone;
 
-bool basicoDrone(_drone d);
+/***********************************************
+criaDrone - Criação de uma instância da estrutura associada ao tipo drone.
+Parametros: matricula - matrícula do drone
+            hora - hora de entrada em operação do drone
+            minuto - minuto de entrada em operação do drone
+Retorno: apontador para a instância criada
+Pre-condições:
+***********************************************/
+drone criaDrone(int capacidade, int alcanceo); 
 
-int idDrone(_drone d);
+/*falta comentar*/
+void destroiDrone(drone d);
 
-int capacidadeCargaDrone(_drone d);
+/***********************************************
+idDrone - Consulta o ID do drone.
+Parametros: d - drone do qual se quer consultar o ID
+Retorno: ID do drone
+Pre-condições: d != NULL
+***********************************************/
+int idDrone(drone d);
 
-int alcanceDrone(_drone d);
+/***********************************************
+capacidadeCargaDrone - Consulta a capacidade de carga do drone.
+Parametros: d - drone do qual se quer consultar a capacidade de carga
+Retorno: capacidade de carga do drone
+Pre-condições: d != NULL
+***********************************************/
+int capacidadeCargaDrone(drone d);
 
-int alcanceDisponivelDrone(_drone d);
+/***********************************************
+alcanceDrone - Consulta o alcance máximo do drone.
+Parametros: d - drone do qual se quer consultar o alcance máximo
+Retorno: alcance máximo do drone
+Pre-condições: d != NULL
+***********************************************/
+int alcanceDrone(drone d);
 
-int restoVooDrone(_drone d);
+/***********************************************
+alcanceDisponivelDrone - Consulta o alcance disponível do drone.
+Parametros: d - drone do qual se quer consultar o alcance disponível
+Retorno: alcance disponível do drone
+Pre-condições: d != NULL
+***********************************************/
+int alcanceDisponivelDrone(drone d);
 
-int restoManutencaoDrone(_drone d);
+/***********************************************
+restoVooDrone - Consulta o tempo restante de voo do drone.
+Parametros: d - drone do qual se quer consultar o tempo restante de voo
+Retorno: tempo restante de voo do drone
+Pre-condições: d != NULL
+***********************************************/
+int restoVooDrone(drone d);
 
-void drone_Base(void);
+/***********************************************
+restoManutencaoDrone - Consulta o tempo restante para a próxima manutenção do drone.
+Parametros: d - drone do qual se quer consultar o tempo restante para a próxima manutenção
+Retorno: tempo restante para a próxima manutenção do drone
+Pre-condições: d != NULL
+***********************************************/
+int restoManutencaoDrone(drone d);
+
+#endif /* DRONE_H_ */
