7f433461e47f913517e81e170a46201dcc343036
Dakeri95 <salvador.costa123.sc@gmail.com>
2024-05-14 18:05:56 +0100

    Update base.c
    o que tenho do base

--- drones/base.c
+++ drones/base.c
@@ -3,47 +3,67 @@
 #include <string.h>
 
 #include "TADs/iterador/iterador.h"
-
+#include "TADs/sequencia/sequencia.h"
 #include "coordenadas.h"
 #include "base.h"
+#include "drone.h"
 
-#define CAP	100
+#define CAP 150
 
-// DRONE BASE
-struct _base{
-	char *nome;
-	coordenadas localizacao;
-	int tempo;//horas
-	
+
+struct _base {
+    char *nome;
+    coordenadas localizacao;
+    int tempo;
+    sequencia drones; 
 };
 
-base criaBase(char *nome, coordenadas localizacao){
-	base b = malloc(sizeof(struct _base));
-	if (b == NULL) return NULL;
-	b->nome = strcpy(malloc(sizeof(nome)+1), nome);
-	b->localizacao = localizacao;
-	// Fazer
-	return b;
+base criaBase(char *nome, coordenadas localizacao) {
+    base b = malloc(sizeof(struct _base));
+    if (b == NULL) return NULL;
+
+    b->nome = strcpy(malloc(strlen(nome) + 1), nome);
+    b->localizacao = localizacao;
+    b->tempo = 0;
+    b->drones = criaSequencia(CAP, (destroiFun)destroiDrone); 
+    return b;
 }
 
-void destroiBase(base b){
-	// Fazer
-	destroiCoordenadas(b->localizacao);
-	free(b->nome);
-	free(b);
+void destroiBase(base b) {
+    destroiSequencia(b->drones); 
+    destroiCoordenadas(b->localizacao);
+    free(b->nome);
+    free(b);
 }
 
-char *nomeBase(base b){
-	return b->nome;
+/*n sei se e necessaria*/
+void destroiBaseEDrones(base b) {
+    destroiSeqEElems(b->drones); 
+    destroiCoordenadas(b->localizacao);
+    free(b->nome);
+    free(b);
 }
 
-coordenadas localizacaoBase(base b){
-	return b->localizacao;
+char *nomeBase(base b) {
+    return b->nome;
 }
 
-void avancaUmaHoraBase(base b, int tempo){
-
-	b->tempo += tempo;
-
+coordenadas localizacaoBase(base b) {
+    return b->localizacao;
 }
 
+void avancaUmaHoraBase(base b, int tempo) {
+    b->tempo += tempo;
+    // Fazer
+}
+
+void adicionaDroneBase(base b, drone d) {
+
+    adicionaPosSequencia(b->drones, d, tamanhoSequencia(b->drones));
+    // (acho que nao usei a seq certa)
+}
+
+/*n sei se e preciso*/
+iterador iteradorDaDroneBase(base b){
+    return iteradorSequencia(b->drones);
+}
