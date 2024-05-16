b77fb321cf4919ffb80879c1935f47bf230e68c2
Dakeri95 <salvador.costa123.sc@gmail.com>
2024-05-16 12:24:53 +0100

    Update encomendas.h

--- drones/encomendas.h
+++ drones/encomendas.h
@@ -1,20 +1,31 @@
-
-/*encomendas.h*/
-
 #ifndef ENCOMENDAS_H_
 #define ENCOMENDAS_H_
 
-
-
-/* Tipo de dados: encomendas */
+/* Tipo de dados: encomenda */
 typedef struct _encomenda *encomenda;
 
-/*coemntar*/
-encomenda criaEncomenda(int peso, coordenadas destino);
+/***********************************************
+criaEncomenda - Cria uma instância da estrutura associada ao tipo encomenda.
+Parâmetros: peso - peso da encomenda, lat - latitude do destino da encomenda, lon - longitude do destino da encomenda
+Retorno: apontador para a instância criada
+Pre-condições: peso > 0
+***********************************************/
+encomenda criaEncomenda(int peso, int lat, int lon);
 
-/*encomendas*/
+/***********************************************
+destroiEncomenda - Liberta a memória ocupada pela instância da estrutura associada ao tipo encomenda.
+Parâmetros: e - encomenda a ser destruída
+Retorno:
+Pre-condições: e!=NULL
+***********************************************/
 void destroiEncomenda(encomenda e);
 
-void cmdBaseEncomenda(char *linha, encomenda e, int *num_encomendas);
+/***********************************************
+cmdBaseEncomenda - Processa o comando para adicionar uma encomenda à base.
+Parâmetros: linha - linha de comando com os parâmetros da encomenda, b - base à qual a encomenda será adicionada, num_encomendas - contador do número de encomendas
+Retorno:
+Pre-condições: linha!=NULL, b!=NULL, num_encomendas!=NULL
+***********************************************/
+void cmdBaseEncomenda(char *linha, base b, int *num_encomendas);
 
-#endif /*ENCOMENDAS_H_*/
+#endif /* ENCOMENDAS_H_ */
