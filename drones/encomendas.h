d77a9287ef764b47b696eb44b452a807467d2cb9
rodrigofusco <rodrigo.fusco2003@gmail.com>
2024-05-16 09:24:15 +0100

    Update encomendas.h

--- drones/encomendas.h
+++ drones/encomendas.h
@@ -1,1 +1,20 @@
 
+/*encomendas.h*/
+
+#ifndef ENCOMENDAS_H_
+#define ENCOMENDAS_H_
+
+
+
+/* Tipo de dados: encomendas */
+typedef struct _encomenda *encomenda;
+
+/*coemntar*/
+encomenda criaEncomenda(int peso, coordenadas destino);
+
+/*encomendas*/
+void destroiEncomenda(encomenda e);
+
+void cmdBaseEncomenda(char *linha, encomenda e, int *num_encomendas);
+
+#endif /*ENCOMENDAS_H_*/
