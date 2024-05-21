565f9fe56c6064b869f4971735d7706ccd4800c7
Dakeri95 <salvador.costa123.sc@gmail.com>
2024-05-21 14:22:00 +0100

    Update base.h

--- drones/base.h
+++ drones/base.h
@@ -13,119 +13,35 @@
 
 typedef struct _base *base;
 
-/***********************************************
-criaBase - Cria uma instância da estrutura associada ao tipo base.
-Parâmetros: nome - nome da base, localizacao - coordenadas da localização da base
-Retorno: apontador para a instância criada
-Pre-condições: localizacao != NULL
-***********************************************/
 base criaBase(char *nome, coordenadas localizacao);
 
-/***********************************************
-destroiBase - Liberta a memória ocupada pela instância da estrutura associada ao tipo base.
-Parâmetros: b - base a ser destruída
-Retorno:
-Pre-condições: b!=NULL
-***********************************************/
 void destroiBase(base b);
 
-/***********************************************
-destroiBaseEDrones - Liberta a memória ocupada pela instância da estrutura associada ao tipo base,
-bem como pelos drones e encomendas nela contidos.
-Parâmetros: b - base a ser destruída
-Retorno:
-Pre-condições: b!=NULL
-***********************************************/
 void destroiBaseEDrones(base b);
 
-/***********************************************
-nomeBase - Consulta o nome da base.
-Parâmetros: b - base da qual se quer consultar o nome
-Retorno: nome da base
-Pre-condições: b!=NULL
-***********************************************/
 char *nomeBase(base b);
-
-/***********************************************
-localizacaoBase - Consulta a localização da base.
-Parâmetros: b - base da qual se quer consultar a localização
-Retorno: coordenadas da localização da base
-Pre-condições: b!=NULL
-***********************************************/
 coordenadas localizacaoBase(base b);
-
-/***********************************************
-avancaUmaHoraBase - Avança o tempo da base em um dado número de horas.
-Parâmetros: b - base cujo tempo será avançado, tempo - número de horas a serem avançadas
-Retorno:
-Pre-condições: b!=NULL, tempo >= 0
-***********************************************/
 void avancaUmaHoraBase(base b, int tempo);
 
-/***********************************************
-adicionaDroneBase - Adiciona um drone à base.
-Parâmetros: b - base à qual o drone será adicionado, d - drone a ser adicionado
-Retorno:
-Pre-condições: b!=NULL, d!=NULL
-***********************************************/
-void adicionaDroneBase(base b, drone d);
-
-/***********************************************
-adicionaEncomendaBase - Adiciona uma encomenda à base.
-Parâmetros: b - base à qual a encomenda será adicionada, e - encomenda a ser adicionada
-Retorno:
-Pre-condições: b!=NULL, e!=NULL
-***********************************************/
+void adicionaDroneBasicoBase(base b, drone d);
+void adicionaDroneColetivoBase(base b, drone d);
 void adicionaEncomendaBase(base b, encomenda e);
 
-/***********************************************
-iteradorDaDroneBase - Cria um iterador para os drones da base.
-Parâmetros: b - base da qual se quer criar o iterador
-Retorno: iterador para os drones da base
-Pre-condições: b!=NULL
-***********************************************/
-iterador iteradorDaDroneBase(base b);
-
-/***********************************************
-iteradorDaEncomendasBase - Cria um iterador para as encomendas da base.
-Parâmetros: b - base da qual se quer criar o iterador
-Retorno: iterador para as encomendas da base
-Pre-condições: b!=NULL
-***********************************************/
-iterador iteradorDaEncomendasBase(base b);
-
-/***********************************************
-daLotacaoBase - Retorna a lotação atual da base (soma de drones e encomendas).
-Parâmetros: b - base da qual se quer obter a lotação
-Retorno: lotação atual da base
-Pre-condições: b!=NULL
-***********************************************/
 int daLotacaoBase(base b);
 
-/***********************************************
-daDroneBase - Retorna o drone na posição i da base.
-Parâmetros: b - base da qual se quer obter o drone, i - índice do drone na base
-Retorno: drone na posição i da base
-Pre-condições: b!=NULL, i deve ser um índice válido
-***********************************************/
-drone daDroneBase(base b, int i);
-
-/***********************************************
-daEncomendasBase - Retorna a encomenda na posição i da base.
-Parâmetros: b - base da qual se quer obter a encomenda, i - índice da encomenda na base
-Retorno: encomenda na posição i da base
-Pre-condições: b!=NULL, i deve ser um índice válido
-***********************************************/
+drone daDroneBasicoBase(base b, int i);
+drone daDroneColetivoBase(base b, int i);
 encomenda daEncomendasBase(base b, int i);
 
-sequencia sequenciaDrones(base b);
+sequencia sequenciaDronesBasicosBase(base b);
+sequencia sequenciaDronesColetivosBase(base b);
+sequencia sequenciaEncomendasBase(base b);
 
-/***********************************************
-cmdListagemBase - Lista todos os drones e encomendas presentes na base.
-Parâmetros: b - base da qual se quer listar os drones e encomendas
-Retorno:
-Pre-condições: b!=NULL
-***********************************************/
-void cmdListagemBase(char *linha, base b);
+bool droneExisteNaBase(base b, int id); //selfExplanatory
+
+//para c 1 2 3 mas 3 e coletivo
+bool idDuplicadoNaBase(base b, int id);
+void removeDroneBase(base b, int id);
+bool droneBasicoEmColetivo(base b, int id);
 
 #endif /* BASE_H_ */
