cef15b0e30ff46c52ed6e0d910ee355637ea06c5
Dakeri95 <salvador.costa123.sc@gmail.com>
2024-05-21 14:20:35 +0100

    Update sistema.c

--- drones/sistema.c
+++ drones/sistema.c
@@ -1,21 +1,31 @@
+/*sistema.c*/
+
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
+#include <stdio.h>
 
 #include "TADs/iterador/iterador.h"
+#include "TADs/sequencia/sequencia.h"
 
 #include "coordenadas.h"
 #include "base.h"
 #include "sistema.h"
+#include "drone.h"
+#include "encomendas.h"
 
 #define TEMPO_ORIGEM	0
+#define CAP 150
+#define MAX_ELEMS_COLETIVO 6
+#define INT_MAX 1500000
+
 
 struct _sistema{
 	base base;
 	int tempoCorrente;
 };
 
-#include <stdio.h>
 
 sistema criaSistema(){
 	sistema s = malloc(sizeof(struct _sistema));
@@ -33,14 +43,81 @@ void destroiSistema(sistema s){
 	free(s);
 }
 
+base daBaseSistema(sistema s){
+    return s->base;
+}
+
 char *nomeBaseSistema(sistema s){
 	return nomeBase(s->base);
 }
 
 int tempoAvancaSistema(sistema s, int n){
+
 	for (int i = 0; i < n; i++){
 		avancaUmaHoraBase(s->base, ++s->tempoCorrente);
 	}
 	return s->tempoCorrente;
 }
 
+void BasicoDroneSistema(sistema s, int capacidade, int alcance, int *num_drones){
+    drone novo_drone = criaDrone(capacidade, alcance);
+    if (novo_drone == NULL) {
+        return;
+    }
+
+    defineCategoriaDrone(novo_drone, "basico");
+    defineIdDrone(novo_drone, ++(*num_drones));
+    adicionaDroneBasicoBase(s->base, novo_drone);
+}
+
+void EncomendasSistema(sistema s, int peso, int lat, int lon, int *num_encomendas){
+    encomenda novaEncomenda = criaEncomenda(peso, lat, lon);
+    if (novaEncomenda == NULL) {
+        return;
+    }
+    
+    defineIdEncomenda(novaEncomenda, (*num_encomendas)++);
+    adicionaEncomendaBase(s->base, novaEncomenda);
+}
+
+drone ColetivoDroneSistema(sistema s, int *ids, int num_ids, int *num_drones) {
+    int capacidade_total = 0;
+    int menor_alcance = INT_MAX;
+    int elems_coletivo[MAX_ELEMS_COLETIVO];
+    base b = daBaseSistema(s);
+    sequencia dronesDaBase = sequenciaDronesBasicosBase(b);
+    int num_elems = 0;
+
+    for (int i = 0; i < num_ids; i++) {
+        drone d = NULL;
+        for (int j = 0; j < tamanhoSequencia(dronesDaBase); j++) {
+            d = elementoPosSequencia(dronesDaBase, j);
+            if (idDrone(d) == ids[i]) {
+                elems_coletivo[num_elems++] = idDrone(d);
+                capacidade_total += capacidadeCargaDrone(d);
+                if(alcanceDrone(d) < menor_alcance) {
+                    menor_alcance = alcanceDrone(d);
+                }
+            }
+        }
+    }
+
+    if (menor_alcance == INT_MAX) {
+    menor_alcance = 0; 
+    }
+
+    drone novo_drone = criaDrone(capacidade_total, menor_alcance);
+    if (novo_drone == NULL) {
+        return NULL;
+    }
+
+    defineCategoriaDrone(novo_drone, "coletivo");
+    defineIdDrone(novo_drone, ++(*num_drones));
+    for (int i = 0; i < num_elems; i++) {
+        novo_drone->elementos[i] = elems_coletivo[i];
+    }
+    novo_drone->numElementos = num_elems;
+    adicionaDroneColetivoBase(s->base, novo_drone);
+
+    return novo_drone;
+}
