577eaa6cbcff3bfe15d06a0bd56708e047745a0c
Dakeri95 <salvador.costa123.sc@gmail.com>
2024-05-21 14:23:05 +0100

    Update sistema.h

--- drones/sistema.h
+++ drones/sistema.h
@@ -4,41 +4,17 @@
 /* Tipo de dados: sistema */
 typedef struct _sistema *sistema;
 
-/* Protótipos das funções associadas a um sistema */
-
-/***********************************************
-criaSistema - Cria uma instância do sistema.
-Retorno: Um ponteiro para a instância criada do sistema
-Precondições: Nenhuma
-***********************************************/
 sistema criaSistema(void);
 
-/***********************************************
-destroiSistema - Libera a memória alocada para uma instância do sistema.
-Parametros:
-    s - O sistema a ser destruído
-Retorno: Nenhum
-Precondições: O sistema não deve ser NULL
-***********************************************/
 void destroiSistema(sistema s);
-
-/***********************************************
-nomeBaseSistema - Obtém o nome da base associada ao sistema.
-Parametros:
-    s - O sistema
-Retorno: O nome da base associada ao sistema
-Precondições: O sistema não deve ser NULL
-***********************************************/
+base daBaseSistema(sistema s);
 char *nomeBaseSistema(sistema s);
 
-/***********************************************
-tempoAvancaSistema - Avança o tempo do sistema em um número específico de horas.
-Parametros:
-    s - O sistema
-    n - O número de horas a avançar
-Retorno: O novo tempo corrente do sistema
-Precondições: O sistema não deve ser NULL
-***********************************************/
 int tempoAvancaSistema(sistema s, int n);
+
+void BasicoDroneSistema(sistema s, int capacidade, int alcance, int *num_drones);
+void EncomendasSistema(sistema s, int peso, int lat, int lon, int *num_encomendas);
+//void ColetivoDroneSistema(sistema s, int num_ids, int *num_drones);
+drone ColetivoDroneSistema(sistema s, int *ids, int num_ids, int *num_drones);
 
 #endif /* _SISTEMA_H_ */
