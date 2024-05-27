8ce1e370f86eb17887858c2d771cf4fc030f644d
Dakeri95 <salvador.costa123.sc@gmail.com>
2024-05-27 16:05:03 +0100

    Update sistema.h

--- drones/sistema.h
+++ drones/sistema.h
@@ -9,12 +9,14 @@ sistema criaSistema(void);
 void destroiSistema(sistema s);
 base daBaseSistema(sistema s);
 char *nomeBaseSistema(sistema s);
-
+int tempoSistema(sistema s);
 int tempoAvancaSistema(sistema s, int n);
 
 void BasicoDroneSistema(sistema s, int capacidade, int alcance, int *num_drones);
-void EncomendasSistema(sistema s, int peso, int lat, int lon, int *num_encomendas);
+void EncomendasSistema(sistema s, int peso, int lat, int lon, int tempoCorrente, int *num_encomendas);
 //void ColetivoDroneSistema(sistema s, int num_ids, int *num_drones);
 drone ColetivoDroneSistema(sistema s, int *ids, int num_ids, int *num_drones);
+int distanciaEncomendaSistema(sistema s, int id);
+void tempoSaidaEncomenda(sistema s, int id, int tempoSaida);
 
 #endif /* _SISTEMA_H_ */
