9fa02ac1df460940fceae4fdd2ff36c1a099f1da
Dakeri95 <salvador.costa123.sc@gmail.com>
2024-05-27 16:06:22 +0100

    Update encomendas.h

--- drones/encomendas.h
+++ drones/encomendas.h
@@ -3,7 +3,7 @@
 
 typedef struct _encomenda *encomenda;
 
-encomenda criaEncomenda(int peso, int lat, int lon);
+encomenda criaEncomenda(int peso, int lat, int lon, int tempoCriacao);
 void destroiEncomenda(encomenda e);
 
 int idEncomenda(encomenda e);
@@ -12,6 +12,7 @@ int latitudeEncomenda(encomenda e);
 int longitudeEncomenda(encomenda e);
 int criacaoEncomenda(encomenda e);
 int saidaEncomenda(encomenda e);
+void defineSaidaEncomenda(encomenda e, int tempoSaida);
 int entregaEncomenda(encomenda e);
 void defineIdEncomenda(encomenda e, int id);
 
